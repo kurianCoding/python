6cf0437538d5f3faee565215ca7277ca1b6cdf3b
kurian <kurian@kurians-MacBook-Pro.local>
2016-09-17 19:39:41 +0530

    Funciton added
    function:miniFunc
    
    description:calculates the minimum number of operations to stringify a
    tree

---
+++ microTree.py
@@ -0,0 +1,22 @@
+def minFunc(arg1):
+    """TODO: Docstring for minFunc.
+
+    :arg1: the tree as an array
+    :returns: minimum number of operations
+
+    """
+    pass
+def optimumFunc(arg1):
+    """TODO: Docstring for optimumFunc.
+
+    :arg1: tree as array
+    :returns: minimum of all the operations 
+
+    """
+    pass
+numberOfTi=raw_input()
+arrayOfTi=[]
+for i in range int(numberOfTi):
+    node=raw_input().split(" ")
+    arrayOfTi[i]=node
+
