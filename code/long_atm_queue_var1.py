decc2e8134691438c6ad4ba5b0594e9be8367849
kurian <kurianck.mail@gmail.com>
2017-02-11 15:01:45 +0530

    variation of the problem
    a slight variation of the problem that i found on hacker earth.

---
+++ code/long_atm_queue_var1.py
@@ -0,0 +1,6 @@
+## this program tries to estimate the maximum number of groups in an 
+## array of integers given that a group has non-decreasing members in that order
+
+## this function takes in the input array of strings and returns an array of integers
+## this function takes an array of integers and returns an integer. It counts the number
+## of instances of non-decrements that happen in the array.
