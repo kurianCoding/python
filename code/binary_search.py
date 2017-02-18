3b4f7611c802b7195f2ab503f6ffa5bdf31e402b
kurian <kurianck.mail@gmail.com>
2017-02-18 07:37:11 +0530

    Fixed problems in binary search
    It turns out the binary search algorithm was working all right, with
    the only problem being the way in which i called it in wrapBinary.I had
    switched the lower and upper binary.

--- code/binary_search.py
+++ code/binary_search.py
@@ -8,7 +8,6 @@
 ## lower limit and a number. It returns the new upper and
 ## lower limits for the array.
 def binarySearch(array,upper_limit,lower_limit,number):
-    print array,upper_limit,lower_limit
     if(array[upper_limit]>number):
         if(array[lower_limit]<number):
                 if(array[(lower_limit+upper_limit)/2]==number):
@@ -16,7 +15,7 @@ def binarySearch(array,upper_limit,lower_limit,number):
                 elif(array[(lower_limit+upper_limit)/2]>number):
                     return binarySearch(array,(upper_limit+lower_limit)/2-1,lower_limit,number)
                 else:
-                    return binarySearch(array,upper_limit,(upper_limit+lower_limit)/2+1,number)
+                    return binarySearch(array,upper_limit,(upper_limit+lower_limit)/2,number)
         elif(array[lower_limit]==number):
             return lower_limit+1
         elif(array[upper_limit]==number):
@@ -29,14 +28,10 @@ def binarySearch(array,upper_limit,lower_limit,number):
 ## the binarySearch function. if it recives a number it prints
 ## the number if it recieves false it will just print 'E'
 def wrapBinary(array,searchTerm):
-    result=binarySearch(array,0,len(array)-1,searchTerm)
+    result=binarySearch(array,len(array)-1,0,searchTerm)
     if(result==False):
         print 'E'
     else:
         print result
     return
-
 wrapBinary([1,2,4,6,10,11,25],1)
-wrapBinary([1,2,4,6,10,11,25],25)
-wrapBinary([1,2,4,6,10,11,25],10)
-wrapBinary([1,2,4,6,10,11,25],6)
