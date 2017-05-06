0cc910cce00a23a9c686f85d4fd96b586b59ab1b
kurian <kurianck.mail@gmail.com>
2017-05-06 07:39:36 +0530

    greedy algo

--- code/selection_greedy.py
+++ code/selection_greedy.py
@@ -1,2 +1,4 @@
-# the program makes the most economic choice
-# when it comes to the next
+# an introduction to greedy algorithms, this is a program
+# which optimizes time. From a given set of tasks it selects
+# the maximum number of tasks which can be done in the mini
+# mum possible time.
